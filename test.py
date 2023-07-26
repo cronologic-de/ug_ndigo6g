7686f49021d891960d0e9a2506aec645402ec11c
SavitaLohana <savitalohana91@gmail.com>
2023-07-26 13:36:28 +0500

    third Commit

--- test.py
+++ test.py
@@ -11,6 +11,6 @@ def convert_tex_to_rst(tex_file, rst_file):
 if __name__ == "__main__":
     # Replace 'example.tex' with the actual path to your LaTeX file
     tex_file_path = 'C:/Users/zeiqbal/PycharmProjects/Ndigov1.1/latex/Ndigo_API.tex'
-    rst_output_path = 'C:/Users/zeiqbal/PycharmProjects/Ndigov1.1/output.rst'  # Replace with the desired output RST file name
+    rst_output_path = 'C:/Users/zeiqbal/PycharmProjects/Ndigov1.1/docs/Ndigo_API.rst'  # Replace with the desired output RST file name
 
     convert_tex_to_rst(tex_file_path, rst_output_path)
